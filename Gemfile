087d583d9aea26bcd933fea8f1bc5052df892879
TomLeWagon <tmarwedel@slb.com>
2022-06-20 07:21:28 -0500

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
@@ -52,3 +54,5 @@ gem 'rails-controller-testing', group: [ :test ]
 gem "autoprefixer-rails", "10.2.5"
 gem "font-awesome-sass", "~> 5.15"
 gem "simple_form", github: "heartcombo/simple_form"
+
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+  before_action :findList, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new # needed to instantiate the form_for
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+
+    if @list.save
+      # no need for app/views/lists/create.html.erb
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def findList
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
